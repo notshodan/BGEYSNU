5e369935907fd8a771d7ae8e85405531f67841d7
pedro <114766590+minsieurmoi@users.noreply.github.com>
2023-10-27 20:45:55 +0200

    finished RTs generation

--- tests/render.c
+++ tests/render.c
@@ -1,1 +1,93 @@
+#include <ctype.h>
+#include <math.h>
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
+typedef struct {
+	SDL_Renderer *renderer;
+	SDL_Window *window;
+} App;
+
+App app;
+
+void prepareScene(){
+	// SDL_SetRenderDrawColor(app.renderer, 96, 128, 255, 255);
+    
+    SDL_RenderDrawLine(app.renderer, 0, 0, 100, 100);
+	SDL_RenderClear(app.renderer);
+}
+
+void presentScene(){
+	SDL_RenderPresent(app.renderer);
+}
+
+void doInput(void){
+	SDL_Event event;
+
+	while(SDL_PollEvent(&event)){
+		switch(event.type){
+			case SDL_QUIT:
+				exit(0);
+				break;
+			default :
+				break;
+		}
+	}
+}
+
+void initSDL(){
+	int rendererFlags, windowFlags;
+	
+	rendererFlags = SDL_RENDERER_ACCELERATED;
+	
+	windowFlags = 0;
+	
+	if(SDL_Init(SDL_INIT_VIDEO) < 0){
+		printf("Couldn't initialize SDL : %s \n", SDL_GetError());
+		exit(1);
+	}
+
+	app.window = SDL_CreateWindow("Test 01", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, windowFlags);
+
+	if(!app.window){
+		printf("Failed to open window : %s \n", SDL_GetError());
+		exit(1);
+	}
+
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+
+	app.renderer = SDL_CreateRenderer(app.window, -1, rendererFlags);
+
+	if(!app.renderer){
+		printf("Failed to create renderer : %s \n", SDL_GetError());
+		exit(1);
+	}
+}
+
+void cleanup(){
+	SDL_DestroyRenderer(app.renderer);
+	SDL_DestroyWindow(app.window);
+	SDL_Quit();
+}
+
+
+int main(int argc, char** argv){
+	memset(&app, 0, sizeof(App));
+
+	initSDL();
+
+	atexit(cleanup);
+
+	while(1){
+		prepareScene();
+		doInput();
+		presentScene();
+		SDL_Delay(16);
+	}
+	return 0;
+}
--- tests/tesselation_draft.c
+++ tests/tesselation_draft.c
@@ -7,7 +7,11 @@
 
 // for now, we only work with regular tilings, ie each tesselation has only one type of tile.
 
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+#include <time.h>
 
 typedef struct cell_s {
     int type;                // number of neighbours
@@ -43,7 +47,10 @@ int GRTS_7_3[3][7] = {
 };
 
 int transition_rules(cell c, int i){  // 0 : P ; -1 : L ; -2 : R ; q > 0 : Child
-        switch(c.type){
+    /*
+    This function only implements Pre-computed GRTS' for {4,5}, {5,4} and {7,3} regular tesselations
+    */
+    switch(c.type){ 
             case 4:
                 return GRTS_4_5[c.state][i];
                 break;
@@ -56,74 +63,133 @@ int transition_rules(cell c, int i){  // 0 : P ; -1 : L ; -2 : R ; q > 0 : Child
             default:
                 return -10;
                 break;
-        }
-    return -10;
+	}
+	return -10;
 }
 
 cell* NewTile(int type, int state){
+    /*
+    Generates a new tile, with all neighbors set to NULL
+    */
     cell* n = malloc(sizeof(cell));
+    assert(n != NULL);
+
     n->type = type;
     n->state = state;
+
     n->move = malloc(sizeof(cell*) * type);
+    assert(n->move != NULL);
+
     for(int i = 0; i < type; i++){
         n->move[i] = NULL;
     }
+
     n->spin = calloc(sizeof(int), type);
 
     return n;
 }
 
-void incr_walker(walker* w, signed int k){
+void incr_walker(walker* w, int k){
+    /*
+    Rotates a walker k times (clockwise if k > 0, else counterclockwise)
+    */
     w->edge = (w->edge + k) % ((w->tile)->type);
+    while(w->edge < 0){
+        w->edge += w->tile->type;
+    }
 }
 
 walker* init_walker(cell* c, int index){
+    /*
+    Initializes a walker looking at the i-th edge of cell c
+    */
     walker* w = malloc(sizeof(walker));
+    assert(w != NULL);
+
     w->tile = c;
     w->edge = index;
     return w;
 }
 
-void step_walker(walker* w){ // w = (t, i), w + STEP := (w->t->move[i], w->t->spin[i])
+void Generate_RTS(cell*, int);
+
+void step_walker(walker* w){
+    /*
+    Steps the walker forward, that is, if w = (t, i), w + STEP := (w->t->move[i], w->t->spin[i])
+    */
     cell* t = w->tile;
     int i = w->edge;
 
-    if(t->move[i] == NULL){
-        t->move[i] = NewTile(t->type, transition_rules(*t, i));
-        t->spin[i] = 0;
+    printf("Walker w = (%d, %d)\n", t->state, i);
 
-        w->tile = t->move[i];
-        w->edge = 0;
+    if(t->move[i] == NULL){
+        /*
+        Case 1 : No tile beyond i-th edge
+        */
+        int s = transition_rules(*t, i);
+
+        if(s > 0){
+            /*
+            If s is positive then i is a tree edge connecting t to a child t' in state s
+            */
+            printf("Creating a tile to step to...\n");
+            t->move[i] = NewTile(t->type, transition_rules(*t, i));
+            t->spin[i] = 0;
+
+            t->move[i]->move[0] = w->tile;
+            t->move[i]->spin[0] = w->edge;
+
+            w->tile = t->move[i];
+            w->edge = 0;
+        }
+        else{
+            /*
+            Else we recursively call Generate_RTS to create all the cells in a 1-tile radius
+            so the walker can move forward
+            */
+            printf("This AIN'T looking fresh, chief... Analyzing the surroundings.\n");
+            Generate_RTS(t, 0);
+            assert(t->move[i] != NULL);
+            w->tile = t->move[i];
+            w->edge = t->spin[i];     
+        }
     }
     else{
+        /*
+        Case 2 : A tile beyond i-th edge already exists
+        */
         w->tile = t->move[i];
         w->edge = t->spin[i];
     }
+    printf("w + STEP = (%d, %d)\n", w->tile->state, w->edge);
 }
 
-void free_tile(cell* c){
-    free(c->move);
-    free(c->spin);
-    free(c);
-}
+void free_tile(cell*);
 
 void Generate_RTS(cell* c, int range){
-    for(int i = 0; i < c->type, i++){
+    printf("\nCurrently in state q = %d\n", c->state);
+    for(int i = 0; i < c->type; i++){
+        printf("Looking at edge index %d/%d\n", i, c->type - 1);
         if(c->move[i] == NULL){
+            printf("No connected tile beyond its %d-th edge !\n", i);
             int x = transition_rules(*c, i);
+            printf("GRTS[%d][%d] = %d\n", c->state, i, x);
             if(x > 0){
-                cell* v = NewTile(c->type, x, c->dist + 1);
+                printf("x = %d > 0 : child connection\n", x);
+                cell* v = NewTile(c->type, x);
 
                 v->spin[0] = i;
                 v->move[0] = c;
                 c->spin[i] = 0;
                 c->move[i] = v;
 
+                printf("Now, current cell is connected to a cell in state %d.\n\n", v->state);
                 if(range > 0){
-                    Generate_RTS(v, tab, range - 1);
+                    Generate_RTS(v, range - 1);
                 }  
             }
             if(x == -1){
+                printf("Current cell is to the left of a non-tree edge. Walking around it.\n");
                 walker* w = init_walker(c, i);
                 incr_walker(w, -1);
 
@@ -141,12 +207,15 @@ void Generate_RTS(cell* c, int range){
                 v->spin[j] = i;
 
                 free(w);
+
+                printf("Now, current cell is connected to a cell in state %d.\n\n", v->state);
             }
             if(x == -2){
+                printf("Current cell is to the right of a non-tree edge. Walking around it.\n");
                 walker* w = init_walker(c, i);
                 incr_walker(w, 1);
 
-                while(transition_rules(*(w->til), w->edge) != -1){
+                while(transition_rules(*(w->tile), w->edge) != -1){
                     step_walker(w);
                     incr_walker(w, 1);
                 }
@@ -160,12 +229,89 @@ void Generate_RTS(cell* c, int range){
                 v->spin[j] = i;
 
                 free(w);
+
+                printf("Now, current cell is connected to a cell in state %d.\n\n", v->state);
+            }
+            if(x == 0){ // PROBLEMS HERE !! 
+                printf("\nParent connection : /!\\ CONSTRUCTION ERROR /!\\\n\n");
             }
         }
         else{
+            printf("This tile already has a connected tile beyond its %d-th edge\n", i);
+            printf("Transition rules : GRTS[%d][%d] = %d\n", c->state, i, transition_rules(*c, i));
             if(range > 0){
-                Generate_RTS(c->move[i], range - 1);
+                if(transition_rules(*c, i) > 0){
+                    printf("Connected tile is in child state! Evaluating it...\n\n");
+                    Generate_RTS(c->move[i], range - 1);
+                }
+                else{
+                    printf("Connected tile is either a parent or a non-tree edge. Skipping it.\n\n");
+                }
+            }
+            else{
+                printf("Next tile out of bounds.\n");
             }
         }
     }
+    printf("Done evaluating this cell.\n\n");
+}
+
+void free_tile(cell* c){
+    for(int i = 0; i < c->type; i++){
+        if(transition_rules(*c, i) >  0 && c->move[i] != NULL){
+            free_tile(c->move[i]);
+        }
+    }
+    free(c->move);
+    free(c->spin);
+    free(c);
+}
+
+void print_RTS(cell* c, int level){
+    if(c == NULL){
+        return;
+    }
+    for(int i = 0; i < level; i++){
+        printf(i == level - 1 ? "  |-" : "  | ");
+    }
+    printf("[ %d ]\n", c->state);
+    for(int i = 0; i < c->type; i++){
+        if(transition_rules(*c, i) > 0){
+            print_RTS(c->move[i], level + 1);
+        }
+    }
+}
+
+// ----- !! -----
+
+
+int main(int argc, char** argv){
+	assert(argc == 3);
+
+	int type = atoi(argv[1]);
+	int range = atoi(argv[2]);
+
+	printf("\nType : %d \nRange : %d \n\n", type, range);
+
+	assert(type == 5 || type == 4 || type == 7);
+    assert(range > 0);
+
+    // clock_t begin = clock();
+	
+	cell* g0 = NewTile(type, 0);
+	
+	Generate_RTS(g0, range - 1);
+
+    // clock_t end = clock();
+    // double duration = (double)(end - begin);
+
+    printf("Successfully generated the RTS\n");
+	
+	print_RTS(g0, 0);
+    // printf("RTS Generated in %.2f ms\n", duration);
+	printf("\n\n");
+
+	free_tile(g0);
+	
+	return 0;
 }
